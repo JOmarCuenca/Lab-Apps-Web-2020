3c861c464e60c55b8abe56ecd9e7dcf3dd6ee6c6
Bruno Mendez <bruno.m.275@gmail.com>
2021-05-03 15:52:43 -0500

    configuracion

--- project/src/Components/Configuracion/index.tsx
+++ project/src/Components/Configuracion/index.tsx
@@ -1,10 +1,12 @@
-import React, { FC, useContext, useEffect, useState } from "react";
-import { Card, Col, Form, Image } from "react-bootstrap";
+import React, { FC, useContext, useEffect, useRef, useState } from "react";
+import { Card, Col, Form, Image, Modal, Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 import { FirebaseContext } from "../../API/Firebase";
 import { Usuario } from "../../Constants/interfaces";
 import profilepicture from "../../Assets/img/profilepicture.png";
 
 import "./style.css";
+import { Input } from "@material-ui/core";
 
 interface Props {
 	setBreadCrumb: (val: string) => void;
@@ -12,12 +14,17 @@ interface Props {
 }
 
 const Configuracion: FC<Props> = ({ setBreadCrumb, usuario }) => {
+	// const { id } = useParams<{ id: string }>();
 	const [user, setUser] = useState<Usuario>(usuario);
 	const [image, setImage] = useState<File | undefined>();
+	const [showModal, setShowModal] = useState(false);
+	const modalEmailRef = useRef<HTMLInputElement>(null);
+	const modalPasswordRef = useRef<HTMLInputElement>(null);
 
 	const [loadingSubmit, setLoadingSubmit] = useState<boolean>(false);
 
 	const firebase = useContext(FirebaseContext);
+	const history = useHistory();
 
 	useEffect(() => {
 		setBreadCrumb("Configuración");
@@ -31,40 +38,100 @@ const Configuracion: FC<Props> = ({ setBreadCrumb, usuario }) => {
 			return;
 		}
 		setImage(files.item(0)!);
+		console.log(files.item(0)?.name);
 		const imgSrc = (window.URL || window.webkitURL).createObjectURL(
 			files.item(0)!
 		);
+		console.log(imgSrc);
 		return imgSrc;
 	};
 
+	const submitModal: React.FormEventHandler = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const email = modalEmailRef.current!.value;
+		const password = modalPasswordRef.current!.value;
+		setLoadingSubmit(true);
+		firebase.doSignInWithEmailAndPassword(email, password).then(() => {
+			setShowModal(false);
+			let message = "Se ha actualizado la información";
+			try {
+				const copy = user!;
+				if (image !== undefined) {
+					copy.imgFile = image;
+				}
+				if (user.email !== usuario.email) {
+					firebase.changeUserEmail(user.email);
+				}
+				firebase.updateUsuario(copy);
+			} catch (e) {
+				console.log(e);
+				message =
+					"Ha ocurrido un error, revise que toda la información sea correcta,\nY que tiene buena conexión de internet.";
+			}
+			setLoadingSubmit(false);
+			window.alert(message);
+			history.push("/dashboard/configuracion");
+		})
+	}
+
 	const submitChanges = async (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
-		setLoadingSubmit(true);
-		let message = "Se ha actualizado la información";
-		try {
-			const copy = user!;
-			if (image !== undefined) {
-				copy.imgFile = image;
+		if (user.email !== usuario.email) {
+			setShowModal(true);
+		} else {
+			setLoadingSubmit(true);
+			let message = "Se ha actualizado la información";
+			try {
+				const copy = user!;
+				if (image !== undefined) {
+					copy.imgFile = image;
+				}
+				await firebase.updateUsuario(copy);
+				console.log(copy);
+			} catch (e) {
+				console.log(e);
+				message =
+					"Ha ocurrido un error, revise que toda la información sea correcta,\nY que tiene buena conexión de internet.";
 			}
-			await firebase.updateUsuario(copy);
-		} catch (e) {
-			console.log(e);
-			message = "Ha ocurrido un error, revise que toda la información sea correcta,\nY que tiene buena conexión de internet.";
+			setLoadingSubmit(false);
+			window.alert(message);
+			history.push("/dashboard/configuracion");
 		}
-		setLoadingSubmit(false);
-		window.alert(message);
 	};
 
 	const renderItem = () => {
+		console.log(usuario);
 		var imgSrc = usuario.imagen_perfil;
 		var inputElement: HTMLInputElement;
 		var imgElement: HTMLImageElement;
 		return (
 			<div>
+				<Modal show={showModal}>
+					<Modal.Header>
+						<Modal.Title>
+							Login
+						</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						<Form.Group onSubmit={submitModal}>
+							<Form.Label>Correo: </Form.Label>
+							<Form.Control type="text" ref={modalEmailRef} />
+						</Form.Group>
+						<Form.Group>
+							<Form.Label>Contraseña: </Form.Label>
+							<Form.Control type="password" ref={modalPasswordRef} />
+						</Form.Group>
+					</Modal.Body>
+					<Modal.Footer>
+						<Button variant="secondary" onClick={() => setShowModal(false)}>
+							Cancelar
+						</Button>
+						<Button variant="primary" type="submit" onClick={() => setShowModal(false)}>
+							Confirmar
+						</Button>
+					</Modal.Footer>
+				</Modal>
 				<Card style={{ borderRadius: 10 }}>
-					<Card.Header>
-						<Card.Title as='h5'>Información</Card.Title>
-					</Card.Header>
 					<Card.Body>
 						<Form onSubmit={submitChanges}>
 							<Form.Row
@@ -134,7 +201,7 @@ const Configuracion: FC<Props> = ({ setBreadCrumb, usuario }) => {
 									/>
 								</Form.Group>
 								<Form.Group as={Col} xs={12} xl={6}>
-									<Form.Label>Email</Form.Label>
+									<Form.Label>email</Form.Label>
 									<Form.Control
 										onChange={(str) => {
 											setUser({
