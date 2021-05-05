8f7e12ab8a8958d57e9080540cb9c18e87067093
Jesus Omar <jesomar.cuenca@gmail.com>
2021-05-05 14:33:44 -0500

    Moved the Graphs folder to correct location Refoctor the name

--- project/src/Components/Stats/index.tsx
+++ project/src/Components/Stats/index.tsx
@@ -1,8 +1,9 @@
 import React, { FC, useContext, useEffect, useState } from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, OverlayTrigger, Row, Tooltip } from "react-bootstrap";
 import { FirebaseContext } from "../../API/Firebase";
 import { STATS_WIDGET_COLORS } from "../../Constants/constants";
 import { StatisticObj } from "../../Constants/interfaces";
+import StatsGraphsScreen from "./Graphs";
 import StatWidget from "./statsWidget";
 
 import "./style.css";
@@ -14,10 +15,9 @@ const StatsScreen : FC<Props> = ({ setBreadCrumb }) => {
 
     const firebase = useContext(FirebaseContext);
 
-    const [
-        stats
-        ,setStats
-    ] = useState<StatisticObj[]>([]);
+    const [stats,setStats]              = useState<StatisticObj[]>([]);
+    const [showGraphs,setShowGraphs]    = useState(false);
+
 
     const [loaded,setLoaded] = useState(false);
 
@@ -40,10 +40,27 @@ const StatsScreen : FC<Props> = ({ setBreadCrumb }) => {
         {createStatsWidgets()}
     </Row>;
 
+    const renderTooltip = (props : any) => (
+        <Tooltip id="button-tooltip" {...props}>
+          Click para cambiar a Graficas
+        </Tooltip>
+      );
+
     const filterBar = () => <div id="FilterBar">
         <div id="FilterHead">Filtrar por</div>
-        <div id="Filter">Esta Semana</div>
+        <OverlayTrigger
+            placement="auto"
+            delay={{ show: 100, hide: 200 }}
+            overlay={renderTooltip}
+        >
+            <div id="Filter" onClick={() => setShowGraphs(true)} >Esta Semana</div>
+        </OverlayTrigger>
     </div>;
+
+    const renderRecentStats = [
+        <Col md={8}><div className="statsWindow">{createStats()}</div></Col>,
+        <Col md={4}>{filterBar()}</Col>
+    ];
 
     const renderStats = () => <Row>
         <Col xs={12}>
@@ -53,8 +70,7 @@ const StatsScreen : FC<Props> = ({ setBreadCrumb }) => {
                 </div>
             </div>
         </Col>
-        <Col md={8}><div className="statsWindow">{createStats()}</div></Col>
-        <Col md={4}>{filterBar()}</Col>
+        {showGraphs ? <StatsGraphsScreen goBack={() => setShowGraphs(false)} /> : renderRecentStats}
     </Row>;
 
     const renderLoading = () => <div><h1>Loading...</h1></div>
