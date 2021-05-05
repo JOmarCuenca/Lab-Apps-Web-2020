1db81f7e8d5109106c31245976b121db2c66786c
Jesus Omar <jesomar.cuenca@gmail.com>
2021-05-05 13:46:41 -0500

    Upload Statisctics Test uploading functions

--- extras/built/index.js
+++ extras/built/index.js
@@ -15,7 +15,7 @@ async function uploadStatistics() {
         {value : "86%", description : "Asistentes a eventos esta semana"},
         {value : "220", description : "Usuarios activos esta semana"},
         {value : "4/5", description : "Bienestar de los usuarios promedio"},
-        {value : "ZEN", description : "Meditación mas popular esta semana"}
+        {value : "ZEN", description : "Meditación mas popular esta semana"},
     ];
     docs.forEach((d,i) => {
         const temp = {
@@ -95,6 +95,89 @@ async function getLimited(){
 // uploadNotif().then(() => console.log("Done"));
 // getLimited();
 
+const STATS_CATEGORIES = [
+    "activeUsersPercent",     // Divide by the total number of users in the Db (Percentage)
+    "totalActiveUsers",       // Number of active Users per Week  (Number)
+    "avgWellBeing",           // Int over 5
+    "mostPopularMeditation"   // Enum value (Yet to be determined)
+]
+
+const STATS_GENERAL_OBJ = {
+    "activeUsersPercent"    : {category : 0, description : "Asistentes a eventos esta semana"},
+    "totalActiveUsers"      : {category : 1, description : "Usuarios activos esta semana"},
+    "avgWellBeing"          : {category : 2, description : "Bienestar de los usuarios promedio"},
+    "mostPopularMeditation" : {category : 3, description : "Meditación mas popular esta semana"}
+}
+
+const MEDITATION_TYPES = [
+    "ZEN",
+    "SHINTO",
+    "BUDHA",
+    "TIBET",
+    "CHAKRA",
+    "PSY"
+]
+
+const WEEK_NUM = 1*7*24*60*60*1000;
+
+function randomStat(i){
+    const random = Math.random();
+    switch(i){
+        case 0: // Percentage of active Users
+            return random*100;
+        case 1: // Number of active users in the DB
+            return Math.round(random*100000);
+        case 2: // Avg Well being of the users
+            return Math.floor((random*100)%5 + 1);
+        default: // Most popular Meditation
+            return Math.floor((random*100)%(MEDITATION_TYPES.length));
+    }
+}
+
+function genRandomStatObj(i){
+    return {
+        ...STATS_GENERAL_OBJ[STATS_CATEGORIES[i]],
+        value : randomStat(i)
+    }
+}
+
+async function uploadStatsTest(){
+    const now = new Date();
+    const futureWeeks = 4, pastWeeks = 8;
+    let batch, counter = 0;
+    for(var i = 0 ; i < futureWeeks ; i++){
+        if(counter%200 === 0){
+            if(batch){
+                await batch.commit();
+            }
+            batch = db.batch();
+        }
+        for(var s = 0; s < STATS_CATEGORIES.length ; s++){
+            batch.set(db.collection("Statistics").doc(`stats_test_future_${i}_${s}`),{
+            ...genRandomStatObj(s),
+            createdDate : new Date(now.getTime() + WEEK_NUM * (i+1))
+        });
+        }
+        counter++;
+    }
+    for(var i = 0 ; i < pastWeeks ; i++){
+        if(counter%200 === 0){  
+            if(batch){
+                await batch.commit();
+            }
+            batch = db.batch();
+        }
+        for(var s = 0; s < STATS_CATEGORIES.length ; s++){
+            batch.set(db.collection("Statistics").doc(`stats_test_past_${i}_${s}`),{
+            ...genRandomStatObj(s),
+            createdDate : new Date(now.getTime() - WEEK_NUM * i)
+        });
+        }
+        counter++;
+    }
+    await batch.commit();
+}
+
 async function setActiveUsers(){
     const number = 200;
     let batch;
@@ -111,10 +194,12 @@ async function setActiveUsers(){
 }
 
 function mainFunction(){
-    const future = setActiveUsers();
+    const future = uploadStatsTest();
     future
     .catch((e) => console.log(e))
     .finally(() => console.log("Done"));
+    // const now = new Date();
+    // console.log(`A week into the future is ${new Date(now.getTime() + WEEK_NUM)}`)
 }
 
 mainFunction();
