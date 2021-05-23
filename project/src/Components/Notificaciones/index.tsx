3ccf9a836413758173a2c61a8d23f9dfdf1ea75d
Jesus Omar <jesomar.cuenca@gmail.com>
2021-05-23 18:58:11 -0500

    Notifications rewrite For no Memory Leaks and others

--- project/src/Components/Notificaciones/index.tsx
+++ project/src/Components/Notificaciones/index.tsx
@@ -105,54 +105,83 @@ const NotificationForm: FC = () => {
 		}
     }
 
+	const renderFormGroup = (label: string, onChange : (s : any) => void, value?: string | number | string[], type? : string, placeholder? : string) => {
+
+		if(type && type === "textarea")
+			return <Form.Group as={Row}>
+				<Form.Label as={Col} lg={12} xl={4} style={{marginBottom : 12}} >{label}</Form.Label>
+				<Col lg={12} xl={8} >
+					<Form.Control
+						className="select"
+						as="textarea"
+						rows={3}
+						placeholder={placeholder}
+						onChange={onChange}
+						value={value}
+						required
+					/>
+				</Col>
+			</Form.Group>;
+
+		return <Form.Group as={Row}>
+			<Form.Label as={Col} xs={12} xl={4} style={{marginBottom : 12}} >{label}</Form.Label>
+			<Col xs={12} xl={8} >
+				<Form.Control
+					className="select"
+					type={type}
+					placeholder={placeholder}
+					onChange={onChange}
+					value={value}
+					required
+				/>
+			</Col>
+		</Form.Group>;
+	}
+
 	const renderItem = () => {
 		return (
 			<Row>
-				<Col xl={6} xs={12}>
-					<h4 className="usuario">Notificación</h4>
-				<div className="maindiv">
-                    <Form onSubmit={submitChanges}>
-                        <p className="parrafo" id="titulo">Titulo</p>
-                        <Form.Control
-							className="select"
-							type="text"
-							placeholder="Titulo de la Notificacion"
-							onChange={(target) => setItem({...item!,title : target.currentTarget.value})}
-							value={item!.title}
-						/>
-                        <hr className="hr1"></hr>
-                        <p className="parrafo" id="mensaje">Mensaje Adjuntado</p>
-                        <Form.Control
-							className="select"
-							as="textarea"
-							placeholder="Ej. Traer sus propios alimentos"
-							rows={3}
-							onChange={(target) => setItem({...item!,descripcion : target.currentTarget.value})}
-							value={item!.descripcion}
-						/>
-                        <br />
-                        <p className="parrafo" id="fecha">Fecha</p>
-                        <Form.Control
-							className="select2"
-							onChange={(str) => {
-								setItem({
-									...item!,
-									fecha: new Date(str.currentTarget.value),
-								});
-							}}
-							required={true}
-							type='date'
-						/>
-                        <hr className="hr1"></hr>
-						<button type="submit" className="publicar">PUBLICAR</button>
-					</Form>
-            	</div>
+				<Col xs={12} xl={6}>
+					<h4 id="notifTag">Notificación</h4>
+					<div className="maindiv">
+						<Form onSubmit={submitChanges}>
+							{renderFormGroup(
+								"Titulo",
+								(target) => setItem({...item!,title : target.currentTarget.value}),
+								item!.title,
+								"text",
+								"Titulo de la Notificacion"
+							)}
+							<hr className="hr1"></hr>
+							{renderFormGroup(
+								"Mensaje Adjunto",
+								(target) => setItem({...item!,descripcion : target.currentTarget.value}),
+								item!.descripcion,
+								"textarea",
+								"Ej. Buscar horario para meditar"
+							)}
+							<br />
+							{renderFormGroup(
+								"Fecha",
+								(str) => {
+									setItem({
+										...item!,
+										fecha: new Date(str.currentTarget.value),
+									});
+								},
+								undefined,	
+								"date"
+							)}
+							<hr className="hr1"></hr>
+							<button type="submit" className="publicar">PUBLICAR</button>
+						</Form>
+					</div>
 				</Col>
 				<Col xl={1}>
 				</Col>
 				<Col xl={4} xs={12}>
 					<h4 className="h">Historial</h4>
-					<div className="maindiv1 overflow-auto">
+					<div id="historicalDiv" className="overflow-auto">
 						<input 
 							type="text" 
 							id = "searchInput"
